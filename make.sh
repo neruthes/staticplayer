6b56539291b2a18752c50acd1be238cbcf7edfe1
Neruthes <i@neruthes.xyz>
2022-06-17 08:15:36 +0000

    improve build script (2)

--- make.sh
+++ make.sh
@@ -9,8 +9,13 @@ case $1 in
         ;;
     install)
         touch install_dirs.txt
+        install_dirs="$(cat install_dirs.txt)"
+        if [[ -z "$install_dirs" ]]; then
+            echo "[ERROR] No installation destination found. Please specify at least one in 'install_dirs.txt'."
+            exit 0
+        fi
         IFS=$'\n'
-        for idir in $(cat install_dirs.txt); do
+        for idir in $install_dirs; do
             echo "[INFO] Installing 'player.html' to '$idir'."
             cat $REPODIR/dist/player.html > $idir/player.html
             cd $idir && find | cut -c3- | sort > .find.txt
